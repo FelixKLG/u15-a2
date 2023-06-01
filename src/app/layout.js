11d6ffa72bca2fae82cdc526f7551e6b378ba35b
FelixKLG <felix@felixklg.dev>
2023-06-01 05:32:48 +0100

    feat: :chart_with_upwards_trend: Umami

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,6 @@
+import Script from "next/script";
 import "./globals.css";
+
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -11,6 +13,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
+			<Script
+				strategy="afterInteractive"
+				async={true}
+				src="https://aly.felixklg.dev/VwDrVUy9RFqbFd6"
+				data-website-id="5cc477cc-029c-463f-b859-7e008595bcfd"
+			/>
 			<body className={inter.className}>{children}</body>
 		</html>
 	);
